5b7caf95837438a6b2a8a4ab9610fa5192289853
gokul <gokul.uf@gmail.com>
2018-09-05 21:18:43 +0200

    added extra timeout argument to init, queue.get timeout to break from the infinite loop

--- worker.py
+++ worker.py
@@ -1,27 +1,34 @@
 from __future__ import print_function
 from multiprocessing import Process
+from queue import Empty
 import time
 
 
 class Worker(Process):
 
-    def __init__(self, gpu_id, queue, lock):
+    def __init__(self, gpu_id, queue, lock, timeout=5):
         super(Worker, self).__init__()
         self.gpu_id = gpu_id
         self.queue = queue
         self.lock = lock
+        self.timeout = timeout
 
     def run(self):
         with self.lock:
             print(f"Worker {self.gpu_id} starting")
 
         while True:
-            job_id, job = self.queue.get()
+            try:
+                job_id, job = self.queue.get(timeout=self.timeout)
+            except Empty:
+                with self.lock:
+                    print(f"(GPU {self.gpu_id}): exiting")
+                    break
             with self.lock:
                 print(f"(GPU {self.gpu_id}): running {job}")
 
             # TODO run the job here
-            time.sleep(10)  # sleep for now
+            time.sleep(2)  # sleep for now
 
             with self.lock:
                 print(f"job {job_id} completed")
