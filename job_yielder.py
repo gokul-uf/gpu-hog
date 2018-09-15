3d0fb021759f6957f46b8176adc13cd574e2faeb
gokul <gokul.uf@gmail.com>
2018-09-15 17:11:24 +0200

    delegate generating jobs to job_yielder which can either take a file or method

---
+++ job_yielder.py
@@ -0,0 +1,30 @@
+import os
+import sys
+
+# method that generates jobs from either
+# a file or another user-defined generator
+
+
+def get_jobs(opt):
+    if opt.job_file:  # we have a job_file
+        with open(opt.job_file) as f:
+            for line in f:
+                line = line.strip()
+                # ignore comments and empty lines
+                if line.startswith("#") or len(line) == 0:
+                    continue
+                yield line
+    else:  # we have job generator
+        directory, module_name = os.path.split(opt.job_method)
+        module_name = os.path.splitext(module_name)[0]
+
+        sys.path.insert(0, directory)
+        try:
+            module = __import__(module_name)
+            module.yielder  # testing if yielder method exists
+        except ImportError:
+            raise ImportError(
+                f"Can't import {opt.job_method}, ensure it exists",
+                " and has a method named 'yielder'")
+
+        yield from module.yielder()
